9bc98c960f0ad6d3804b8f0d6647a7d2d804f9c7
Taylor Smith <taylors@lionsoft.net>
2018-05-22 12:36:17 -0500

    -- Allow for connecting to databases without passwords

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 //Dependencies
+import _ from 'lodash';
 import Logger from 'winston';
 import dotenv from 'dotenv';
 import moment from 'moment';
 import mongoose from 'mongoose';
+import path from 'path';
 
 //Local Dependencies
 import App from './app';
@@ -12,7 +14,11 @@ const logFileTimeFormat = `YYYYMMDD-hh:mm:ss.SSa`;
 const logFileTimestamp = moment().format(logFileTimeFormat);
 
 //Initialization
+//Read in env variables
 dotenv.config();
+
+//Set project root path
+process.env.PROJECT_ROOT = path.normalize(`${__dirname}/../../`);
 
 const app = new App();
 
@@ -20,8 +26,18 @@ if (process.env.LOG_TO_FILE === `true`) {
 	Logger.add(Logger.transports.File, { filename: `api-server.${logFileTimestamp}.log` });
 }
 
-const mongoConnectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_USER_PASS}@${process.env.DB_SERVER}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH_DB || `admin`}`;
-mongoose.connect(mongoConnectionString);
+const mongoConnectionString = `mongodb://${process.env.DB_SERVER}:${process.env.DB_PORT}`;
+const mongoConnectionOptions = {
+	dbName: process.env.DB_NAME,
+};
+
+//If we have a db user and password provided, use them to authenticate
+if (_.trim(process.env.DB_USER) !== `` && _.trim(process.env.DB_USER_PASS) !== ``) {
+	mongoConnectionOptions.user = process.env.DB_USER;
+	mongoConnectionOptions.pass = process.env.DB_USER_PASS;
+}
+
+mongoose.connect(mongoConnectionString, mongoConnectionOptions);
 const db = mongoose.connection;
 
 /* check to see if we`re connected */
