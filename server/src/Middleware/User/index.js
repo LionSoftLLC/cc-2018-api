c7f1e2ee82e6f7f6c6ea52dbfa99caf6d17f07b3
Taylor Smith <taylors@lionsoft.net>
2018-05-26 17:11:50 -0500

    -- Added AcceptFriend to User middleware exports

--- server/src/Middleware/User/index.js
+++ server/src/Middleware/User/index.js
@@ -1,4 +1,5 @@
 export { AddFriend } from './AddFriend';
+export { AcceptFriend } from './AcceptFriend';
 export { RemoveFriend } from './RemoveFriend';
 export { UpdateProfile } from './UpdateProfile';
 export { UploadProfileImage } from './UploadProfileImage';
