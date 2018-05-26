b497e50477336188a962dd8d2a2fa5567b25de6f
Taylor Smith <taylors@lionsoft.net>
2018-05-26 17:10:16 -0500

    -- Updated HandleMediaUpload to ensure the destination file dir exists

--- server/src/Middleware/Content/HandleMediaUpload.js
+++ server/src/Middleware/Content/HandleMediaUpload.js
@@ -1,13 +1,15 @@
 //Dependencies
+import Logger from 'winston';
 import multer from 'multer';
 import Uuid from 'uuid';
 import moment from 'moment';
 import mime from 'mime';
+import fs from 'fs-extra';
 
 //Instantiated
 /* eslint-disable no-magic-numbers */
 const multerConfig = {
-	maxFileSize: 5 * 1024 * 1024, // no larger than 5mb,
+	maxFileSize: 15 * 1024 * 1024, // no larger than 15mb,
 	fileField: `file`,
 };
 /* eslint-enable no-magic-numbers */
@@ -16,8 +18,17 @@ const upload = multer({
 	storage: multer.diskStorage({
 		destination: (req, file, cb) => {
 			const mediaType = file.mimetype.split(`/`)[0];
+			const destinationPath = `${process.env.PROJECT_ROOT}/server/static/media/${mediaType}s`;
 
-			cb(null, `${process.env.PROJECT_ROOT}/server/static/media/${mediaType}s`);
+			fs.ensureDir(destinationPath)
+				.then(() => {
+					cb(null, destinationPath);
+				})
+				.catch((err) => {
+					Logger.error(`Unable to ensure static media directory exists`);
+					Logger.error(err);
+					cb(err, null);
+				});
 		},
 		filename: (req, file, cb) => {
 			const fileId = `${Uuid.v4()}-${moment.now()}`;
